fa095ae89ced280039ab4ae8d0551be69635d462
ZaraBlu <bigdeluzahra@yahoo.com>
2024-06-20 12:13:52 +0330

    Mosh-first-commit

---
+++ mosh-first-prj/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Message from './Messages';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ mosh-first-prj/src/Messages.tsx
@@ -0,0 +1,7 @@
+function message() {
+  const name = 'Zara';
+  if (name) return <h1>Hello ts React from {name}</h1>;
+  return <h1>Hello ts React From Anonimouse</h1>;
+}
+
+export default message;
